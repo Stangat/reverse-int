160bd559ff74fc2415c82a200e4cb2c0e756aad5
Stanislau Hatsilau <stasgat12@gmail.com>
2022-07-13 20:19:47 +0300

    feat: implement function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  const nToStr = Math.abs(n).toString();
+  let revNum = '';
+  for (let i = 1; i <= nToStr.length; i++) {
+    let lastChar = nToStr.charAt(nToStr.length - i);
+    revNum += lastChar; 
+  }
+  return +revNum;
 }
